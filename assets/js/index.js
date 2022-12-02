fd70107cd41a2c6eecd9fcbbdbcb8974aee84e63
Luca <lucagcuello@gmail.com>
2022-12-01 21:35:14 -0300

    added socials, changed background color

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+const song = new Audio("assets/sounds/love.mp3"),
+  play = new Audio("assets/sounds/play.mp3"),
+  button = document.getElementById("play"),
+  buttonPause = document.getElementById("pause"),
+  circle1 = document.getElementById("circle1"),
+  circle2 = document.getElementById("circle2"),
+  innerCircle1 = document.getElementById("inner-circle"),
+  innerCircle2 = document.getElementById("inner-circle2");
+
+const init = () => {
+  button.addEventListener("click", function () {
+    song.play();
+    play.play();
+    button.style.display = "none";
+    buttonPause.style.display = "flex";
+    circle1.style.animationPlayState = "running";
+    circle2.style.animationPlayState = "running";
+    innerCircle1.style.animationPlayState = "running";
+    innerCircle2.style.animationPlayState = "running";
+  });
+  buttonPause.addEventListener("click", function () {
+    song.pause();
+    play.play();
+    button.style.display = "flex";
+    buttonPause.style.display = "none";
+    circle1.style.animationPlayState = "paused";
+    circle2.style.animationPlayState = "paused";
+    innerCircle1.style.animationPlayState = "paused";
+    innerCircle2.style.animationPlayState = "paused";
+  });
+};
+
+init();
